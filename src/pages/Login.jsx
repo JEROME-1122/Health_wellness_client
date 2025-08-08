ddce636bbe407aceb0dfb16c73b27b77ab23f05e
Jerome Franklin X <118640107+JEROME-1122@users.noreply.github.com>
2025-08-09 00:16:59 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -45,9 +45,9 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
           className="w-full p-2 mb-4 border rounded"
         />
-{/*             <Link to="/reset-password" className="text-blue-500 hover:underline  ">
+            <Link to="/reset-password" className="text-blue-500 hover:underline  ">
           <p className="mb-4">forgot Passowrd</p>
-        </Link> */}
+        </Link>
         <button
           type="submit"
           className="w-full bg-green-500 text-white py-2 rounded hover:bg-green-600"
@@ -66,5 +66,6 @@ const Login = () => {
 };
 
 export default Login;
+
 
 
